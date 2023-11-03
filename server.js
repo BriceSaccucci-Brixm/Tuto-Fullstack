4184394273ffed9e079b19296439d9fa52ec9dc7
BriceSaccucci-Brixm <bricesaccucci@gmail.com>
2023-11-03 16:27:15 +0100

    Initial Author routes

--- server.js
+++ server.js
@@ -5,14 +5,17 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
 
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false}))
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.MONGODB_URL)
@@ -21,8 +24,9 @@ db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))
 
 
+// Routes
+app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
-
-app.use('/',indexRouter)
-
+// Listen
 app.listen(process.env.PORT || 3000)
